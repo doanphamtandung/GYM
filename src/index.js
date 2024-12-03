6eca25e75a6d15177d548f57c7b8d779b6c0185d
Doanphamtandung2005 <doanphamtandung2003@gmail.com>
2024-12-03 08:27:47 +0700

    up file

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <Provider store={store}>
-    <BrowserRouter basename="/gymtes">
+    <BrowserRouter basename="/GYM">
       <App />
     </BrowserRouter>
   </Provider>
